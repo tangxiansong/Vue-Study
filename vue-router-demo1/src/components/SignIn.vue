26ebea276321a1f1e895d622ad180c179533253a
tangxiansong <1526198692@qq.com>
2019-03-27 09:26:23 +0800

    first commit

---
+++ vue-router-demo1/src/components/SignIn.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="container">
+		<h2>登录</h2>
+		{{msg}}
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'Signin',
+		data(){
+			return{
+				msg:'这是登录'
+			};
+		}
+	};
+</script>
+
+<style scoped>
+</style>
---
+++ vue-router-demo1/src/components/Task.vue
@@ -0,0 +1,18 @@
+<template>
+	<div class="container">
+		<h2>用户中心</h2>
+	</div>
+</template>
+
+<script>
+	export default{
+		name:'Task',
+		data(){
+			return{
+			};
+		}
+	};
+</script>
+
+<style scoped>
+</style>
